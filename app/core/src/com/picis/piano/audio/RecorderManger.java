822e76d9ef8a53b78035f0718570223219c68b25
askubyev <askubyev@ozon.ru>
2021-11-16 18:43:00 +0300

    try fix

--- app/core/src/com/picis/piano/audio/RecorderManger.java
+++ app/core/src/com/picis/piano/audio/RecorderManger.java
@@ -1,22 +1,25 @@
 package com.picis.piano.audio;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.AudioRecorder;
+import com.badlogic.gdx.backends.lwjgl.audio.JavaSoundAudioRecorder;
 
 public class RecorderManger {
     private final int samples;
     private final boolean isMono;
-//    private final AudioRecorder recorder;
+    private final AudioRecorder recorder;
 
     public RecorderManger(int samples, boolean isMono) {
         this.samples = samples;
         this.isMono = isMono;
-//        recorder = Gdx.audio.newAudioRecorder(samples, isMono);
+        recorder = new JavaSoundAudioRecorder(samples, isMono);
     }
 
     public short[] record(int millis) {
         final short[] data = new short[(int) (samples * ((float) millis / 1000f))];
-//        recorder.read(data, 0, data.length);
+        recorder.read(data, 0, data.length);
+        for (short datum : data) {
+            System.out.print(datum + " ");
+        }
         return data;
     }
 }
